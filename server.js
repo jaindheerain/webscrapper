4a8f7c7fe5d54dd90079864dd5005d0448600230
dheerain-jain <jain.dheerain@gmail.com>
2017-08-20 23:23:18 +0530

    titleExtracted

--- server.js
+++ server.js
@@ -1,3 +1,47 @@
-/**
- * Created by Dheerain on 20-08-2017.
- */
+
+var cheerio=require('cheerio');
+var request=require('request');
+var fs=require('fs');
+var express=require('express');
+var app=express();
+
+app.get('/scrape',function (req,res) {
+
+    console.log("Entered")
+
+    url = 'http://www.imdb.com/title/tt1229340/';
+
+    request(url,function (error,res,html) {
+
+        console.log(error);
+
+        if(!error){
+
+            console.log(html);
+
+            var $=cheerio.load(html);
+
+            var title,release,rating;
+
+            var json={title:"",release:"",rating:""};
+
+            $('.header').filter(function(){
+
+                var data=$(this);
+                title = data.children().first().text();
+
+                json.title=title;
+                res.send(title);
+                console.log(title);
+            });
+
+        }
+
+    });
+
+});
+var port=9090||process.env.port;
+app.listen(port,function () {
+    console.log("Server datrred");
+});
+
